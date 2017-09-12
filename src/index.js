266ac5ad49bfeb6c258540589262b4a3a557b782
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-09-11 17:13:48 -0700

    Store basics.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import { createStore } from 'redux';
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <App store={store}/>,
+    document.getElementById('root')
+);
